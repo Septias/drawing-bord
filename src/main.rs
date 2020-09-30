8a8afec31b40c48fb1ee06622ebcd525e32f0db6
Septias <39526136+Septias@users.noreply.github.com>
2020-09-30 22:05:41 +0200

    use nalgebra instead of mint for points

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 use ggez::*;
 use ggez::input::mouse::MouseButton;
-use mint::{Point2};
+use nalgebra as na;
+
+// variables
+const CLEAR_COLOR: graphics::Color = graphics::Color::new(0.9, 0.87, 0.72, 1.0); // background-color
+
+type Point2 = na::Point2<f32>;
 
 #[derive(Debug)]
 struct Line{
-    points: Vec<Point2<f32>>,
+    points: Vec<Point2>,
 }
 
 impl Line {
@@ -21,8 +26,8 @@ struct State {
     lines: Vec<graphics::Mesh>
 }
 
-fn distance (p1: &Point2<f32>, p2: &Point2<f32>) -> f32{
-    ((p1.x - p2.x).powf(2.) + (p1.y - p2.y).powf(2.)).sqrt()
+fn distance (p1: &Point2, p2: &Point2) -> f64{
+    ((p1[0] - p2[1]).powf(2.) as f64 + (p1.y - p2.y).powf(2.) as f64).sqrt()
 }
 
 const CLEAR_COLOR: graphics::Color = graphics::Color::new(0.9, 0.87, 0.72, 1.0);
@@ -51,8 +56,8 @@ impl ggez::event::EventHandler for State {
         self.current_line = Some(Line::new());
 
         // push twice because one point is the origin and the second one gets replaced with the mouse position
-        self.current_line.as_mut().unwrap().points.push(Point2{x: _x, y: _y});
-        self.current_line.as_mut().unwrap().points.push(Point2{x: _x + 1., y: _y}); // +1. because they have to be unique
+        self.current_line.as_mut().unwrap().points.push(Point2::new(x, y));
+        self.current_line.as_mut().unwrap().points.push(Point2::new(x + 1., y)); // +1. because they have to be unique
 
 
     }
@@ -66,15 +71,16 @@ impl ggez::event::EventHandler for State {
             let line = self.current_line.as_mut().unwrap();
             line.points.pop();
             let last_point = line.points.last().unwrap().clone();
-            let current_position = mint::Point2{x: _x, y: _y};
-            if distance(&last_point, &current_position) > 5. {
+            let current_position = Point2::new(x, y);
+            if distance(&last_point, &current_position) > 2. {
                 line.points.push(current_position)
             }
             if &current_position == &last_point{
-                line.points.push(Point2{x: _x + 1., y: _y})
+                line.points.push(Point2::new(x, y))
             }
             else {
-                line.points.push(Point2{x: _x, y: _y})
+                line.points.push(Point2::new(x, y))
+            }
             }
         }
     }
@@ -93,14 +99,4 @@ fn main() {
         .window_mode(conf::WindowMode::default().resizable(true)); //.borderless(true)
     let (ref mut ctx, ref mut event_loop) = &mut cb.build().unwrap();
     event::run(ctx, event_loop, state).unwrap();
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    #[test]
-    fn distance_check(){
-        assert_eq!(distance(&Point2{x: 5., y: 0. }, &Point2{x: 0., y: 0.}), 5.);
-        assert_eq!(distance(&Point2{x: 4., y: 3. }, &Point2{x: 0., y: 0.}), 5.);
-    }
 }
